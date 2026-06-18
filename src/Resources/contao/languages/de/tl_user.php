0d8f8a397dd965f3026a950095139b32170cb564
Niels <info@heimseiten.de>
2026-06-18 13:03:55 +0200

    tl_user: Legende uebersetzen (Show CSS in Lists (heimseiten.de)), DE+EN

--- src/Resources/contao/languages/de/tl_user.php
+++ src/Resources/contao/languages/de/tl_user.php
@@ -1,5 +1,7 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_user']['showCssInListsLegend'] = 'Show CSS in Lists (heimseiten.de)';
+
 $GLOBALS['TL_LANG']['tl_user']['viewIdAndCssInArticlesAndContentElements'] = [
     'CSS in Seiten, ID und CSS in Artikeln und Inhaltselementen anzeigen',
     'Zeigt die ID und die CSS-Klassen in den Backend-Labels der Seiten, Artikel und Inhaltselemente an.',
--- src/Resources/contao/languages/en/tl_user.php
+++ src/Resources/contao/languages/en/tl_user.php
@@ -1,5 +1,7 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_user']['showCssInListsLegend'] = 'Show CSS in Lists (heimseiten.de)';
+
 $GLOBALS['TL_LANG']['tl_user']['viewIdAndCssInArticlesAndContentElements'] = [
     'Show CSS on pages and ID/CSS on articles and content elements',
     'Shows the ID and the CSS classes in the back end labels of pages, articles and content elements.',
