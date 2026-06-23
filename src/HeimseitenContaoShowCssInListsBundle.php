ab0106f4a7e63ad5acc8706c5d003fd281e2dfa7
Niels <info@heimseiten.de>
2026-06-23 09:39:06 +0200

    Move to the modern Contao bundle layout (contao/, config/, public/ at the root; AbstractBundle + loadExtension)

--- src/HeimseitenContaoShowCssInListsBundle.php
+++ src/HeimseitenContaoShowCssInListsBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Heimseiten\ContaoShowCssInListsBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class HeimseitenContaoShowCssInListsBundle extends Bundle
+class HeimseitenContaoShowCssInListsBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
